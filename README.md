4da388251a10e3f1247379b99a31873ef6bfebd3
anon <noreply>
2020-12-05 15:04:13 -0500

    added project

---
+++ README.md
@@ -0,0 +1,7 @@
+## Remove live videos from Youtube search results and video recommendations. 
+
+## Load on Chrome
+1. Clone with git or download as zip and extract. 
+2. Go to the Chrome's extension page, or just enter chrome://extensions in the Chrome search bar.
+3. Enable developer mode in the top-right of the page.
+4. Click the load unpacked button and load the extracted folder.
---
+++ content.js
@@ -0,0 +1,45 @@
+function getAll(node, pred) {
+  let nodes = [] 
+
+  if (!node) return nodes 
+
+  if (pred(node)) {
+      nodes.push(node) 
+  }
+
+  node = node.firstElementChild
+  while (true) {
+      if (!node) break 
+      nodes = [...nodes, ...getAll(node, pred)] 
+      node = node.nextElementSibling
+  }
+  return nodes 
+}
+
+const TAGS = ["YTD-COMPACT-VIDEO-RENDERER", "YTD-VIDEO-RENDERER", "YTD-RICH-ITEM-RENDERER"]
+
+function clean(node) {
+  for (let elem of getAll(node, node => TAGS.includes(node.tagName) && node.querySelector(".badge-style-type-live-now"))) {
+      elem.remove()
+  }
+}
+
+
+
+function main() {
+  const obs = new MutationObserver((muts, obs) => {
+    for (let mut of muts) {
+      for (let node of (mut.addedNodes || [])) {
+        clean(node)
+      }
+      
+    }
+  }) 
+
+  obs.observe(document.body, {childList: true, subtree: true})
+
+  clean(document.body)
+}
+
+
+main()
